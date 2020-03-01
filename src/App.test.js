88b15cdeca1eacc4046a5e0846d6dd16d3b01599
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2020-03-01 16:36:01 +0100

    test(frontend): update App test suite

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component tests", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+  });
+  test("renders card holder", () => {
+    const { getByTestId } = render(<App />);
+    const classHolder = getByTestId("options-test");
+    expect(classHolder).toBeInTheDocument();
+  });
 });
