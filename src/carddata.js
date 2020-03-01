55efd9f35578ef2b495ab788083a04bacd91d0ad
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2020-03-01 16:37:13 +0100

    feat(frontend): implement Card component - create helper function

---
+++ src/carddata.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    iconId: "icon-1",
+    h2: "L",
+    main: "Masks",
+    sub: "Don't tear off masks from people, maybe it's a muzzle"
+  },
+  {
+    iconId: "icon-2",
+    h2: "I",
+    main: "Solitude",
+    sub: "Solitude is fine but you need someone to tell that solitude is fine"
+  },
+  {
+    iconId: "icon-3",
+    h2: "F",
+    main: "Kindness",
+    sub: "Be kind. Even on your bad days"
+  },
+  {
+    iconId: "icon-4",
+    h2: "E",
+    main: "Miracles",
+    sub: "Sometimes miracles are just good people with kind hearts"
+  }
+];
